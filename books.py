1fbd5467ee857140fbd1ac59acc96aed02dfaa33
Rafael Gallardo <rafaelgallardo2836@gmail.com>
2024-08-29 20:39:59 -0600

    Modified book description length

--- books.py
+++ books.py
@@ -34,7 +34,7 @@ class Book(BaseModel):
     '''
     title: str = Field(min_length=1)
     author: str = Field(min_length=1, max_length=100)
-    description: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=1000)
     rating: int = Field(gt=-1, lt=101)
 
 
